e16133b16165e6d99ef2d90546eac7435c84171f
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-05-02 23:33:00 -0500

    Test websocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use eyre::Result;
+use tungstenite::accept;
+
+fn main() -> Result<()> {
+    let port_str = std::env::var("MONOPOLY_SERVER_PORT")?;
+
+    let server = TcpListener::bind(format!("0.0.0.0:{}", port_str))?;
+    for stream in server.incoming() {
+        let mut websocket = accept(stream?)?;
+
+        loop {
+            let msg = websocket.read()?;
+
+            if msg.is_binary() || msg.is_text() {
+                websocket.send(msg)?;
+            }
+        }
+    }
+
+
+    Ok(())
 }
