1198d68ac4223ee761186c6a6d1cd1a24e775a02
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-05-25 01:29:28 -0500

    See why the command is failing

--- http/src/main.rs
+++ http/src/main.rs
@@ -28,11 +28,11 @@ async fn create_game(_: Request<()>) -> tide::Result {
     for _ in 0..128 {
         host_key.push(rand::thread_rng().gen_range(b'A'..=b'Z') as char);
     }
-    //
-    // Command::new(std::env::var("MONOPOLY_GAME_BIN_PATH")?)
-    //     .env("MONOPOLY_GAME_PATH", OsStr::from_bytes(&game_code))
-    //     .env("MONOPOLY_HOST_KEY", host_key)
-    //     .spawn()?;
+
+    dbg!(Command::new(std::env::var("MONOPOLY_GAME_BIN_PATH")?)
+        .env("MONOPOLY_GAME_PATH", OsStr::from_bytes(&game_code))
+        .env("MONOPOLY_HOST_KEY", host_key)
+        .spawn())?;
 
     Ok("HELLO WORLD".into())
 }
