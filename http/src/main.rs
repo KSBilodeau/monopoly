181b766daf4d55328f81716c4bb63df865635e68
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-05-25 01:25:50 -0500

    Change path due to routing issues

--- http/src/main.rs
+++ http/src/main.rs
@@ -55,8 +55,7 @@ fn main() -> Result<()> {
         let task_one = async_std::task::spawn(async move {
             let mut server = tide::new();
 
-            server.at("/api/create_game").get(create_game);
-            server.at("/api").get(test);
+            server.at("/api").get(create_game);
 
             let ip_addr = format!("127.0.0.1:{}", std::env::var("MONOPOLY_HTTP_PORT")?);
             server.listen(ip_addr).await?;
