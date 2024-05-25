1e2cd245f1e3d27189234558475747ab58241407
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-05-25 01:11:25 -0500

    Further routing testing

--- http/src/main.rs
+++ http/src/main.rs
@@ -42,6 +42,10 @@ async fn test_sock(mut request: Request<()>) -> tide::Result {
     Ok(request.body_string().await?.into())
 }
 
+async fn test(mut request: Request<()>) -> tide::Result {
+    Ok(request.body_string().await?.into())
+}
+
 fn main() -> Result<()> {
     unsafe {
         libc::signal(libc::SIGCHLD, libc::SIG_IGN);
@@ -52,6 +56,7 @@ fn main() -> Result<()> {
             let mut server = tide::new();
 
             server.at("/api/create_game").get(create_game);
+            server.at("/").get(test);
 
             let ip_addr = format!("127.0.0.1:{}", std::env::var("MONOPOLY_HTTP_PORT")?);
             server.listen(ip_addr).await?;
