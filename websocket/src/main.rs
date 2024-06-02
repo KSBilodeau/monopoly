c77f552eae6dbc9dcd9816e9cff262f86473919d
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2024-06-01 23:40:24 -0500

    Establish working websocket server

--- websocket/src/main.rs
+++ websocket/src/main.rs
@@ -1,28 +1,19 @@
-#![feature(lazy_cell)]
-#![feature(concat_bytes)]
+#![feature(async_closure)]
 #![warn(clippy::pedantic)]
 #![deny(rust_2018_idioms)]
 
 use std::io::Read;
 use std::os::unix::net::SocketAddr;
-use std::sync::{Arc, LazyLock};
 
 use async_std::os::unix::net::{UnixListener, UnixStream};
 use eyre::Result;
 use log::*;
-use parking_lot::Mutex;
 use soketto::handshake::server::Response;
 use soketto::handshake::Server;
-
-use crate::api::back::EventHandler;
-use crate::api::front::CommandHandler;
 
 mod api;
 mod game;
 mod util;
-
-static GAME: LazyLock<Arc<Mutex<game::Session>>> =
-    LazyLock::new(|| Arc::new(Mutex::new(game::Session::new())));
 
 async fn serve_websocket(stream: UnixStream, addr: SocketAddr) -> Result<()> {
     let mut rand_file = std::fs::File::open("/dev/random")?;
@@ -44,6 +35,7 @@ async fn serve_websocket(stream: UnixStream, addr: SocketAddr) -> Result<()> {
             error!("Failed to receive connection request for WS (#{})", ws_id);
             return Ok(());
         };
+
         info!("Received request for path: {}", req.path());
         req.key()
     };
@@ -58,56 +50,19 @@ async fn serve_websocket(stream: UnixStream, addr: SocketAddr) -> Result<()> {
         return Ok(());
     };
 
-    let (sender, receiver) = {
-        let (send, recv) = server.into_builder().finish();
-        (Arc::new(Mutex::new(send)), Arc::new(Mutex::new(recv)))
-    };
+    let (mut send, mut recv) = server.into_builder().finish();
 
-    util::sync!(sender.lock().send_text("CONNECTED")).unwrap();
+    loop {
+        let mut data = vec![];
+        let data_type = recv.receive_data(&mut data).await?;
 
-    let (send, recv) = std::sync::mpsc::channel();
+        if data_type.is_text() {
+            send.send_text(String::from_utf8_lossy(&data).to_string())
+                .await?;
+        }
 
-    let mut comm_handler = CommandHandler::new(ws_id, send, GAME.clone());
-    let mut event_handler = EventHandler::new(ws_id, recv, GAME.clone());
-
-    std::thread::scope(|s| {
-        let send1 = sender.clone();
-        let recv1 = receiver.clone();
-        s.spawn(move || {
-            let sender = send1.clone();
-            let receiver = recv1.clone();
-
-            loop {
-                let command = comm_handler.pump_command(receiver.clone());
-
-                if let Some(command) = command {
-                    comm_handler.execute_command(&command, sender.clone());
-                }
-
-                if comm_handler.is_kill() {
-                    break;
-                }
-            }
-        });
-
-        let send2 = sender.clone();
-        s.spawn(move || {
-            let sender = send2.clone();
-            loop {
-                let event = event_handler.pump_event();
-
-                if let Some(event) = event {
-                    event_handler.execute_event(event, sender.clone());
-                }
-
-                if event_handler.is_kill() {
-                    break;
-                }
-            }
-        });
-    });
-
-    Ok(())
+        data.clear();
+    }
 }
 
 fn main() -> Result<()> {
@@ -128,7 +83,7 @@ fn main() -> Result<()> {
         "MISSING MONOPOLY_CHOWN_ID ENV VAR"
     );
 
-    async_std::task::block_on(async move {
+    async_std::task::block_on(async {
         let sock_addr = std::env::var("MONOPOLY_GAME_PATH")?;
 
         let server = UnixListener::bind(&sock_addr).await?;
@@ -137,7 +92,7 @@ fn main() -> Result<()> {
         std::os::unix::fs::chown(sock_addr, Some(33), Some(33))?;
 
         while let Ok((stream, addr)) = server.accept().await {
-            async_std::task::spawn(serve_websocket(stream, addr));
+            async_std::task::spawn(async { serve_websocket(stream, addr).await });
         }
 
         Ok(())
--- websocket/src/util.rs
+++
@@ -1,7 +0,0 @@
-macro_rules! sync {
-    ($future: expr) => {
-        async_std::task::block_on(async { $future.await })
-    };
-}
-
-pub(crate) use sync;
